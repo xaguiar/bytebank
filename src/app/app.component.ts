4f45e549aede67d37a0287b58fe229caf677970d
unknown <xaguiar@live.com>
2021-10-13 22:44:40 -0300

    alterações services e components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private service: TransferenciaService) {
+
+  }
   title = 'bytebank';
+
+  transferir($event: any) {
+    this.service.adicionar($event)
+  }
 }
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
+  transferir() {
+    console.log('Nova transferencia');
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
